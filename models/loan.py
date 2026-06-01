87f248094317a2e78a582a516374998bd5d7603f
Shahzod Tursinboyev <shahzod.caa@gmail.com>
2026-06-01 21:09:28 +0500

    Implement Loan class

---
+++ models/loan.py
@@ -0,0 +1,14 @@
+class Loan:
+    """Represents a library loan - a member borrowing a book."""
+    
+    def __init__(self, member, book, borrowed_date):
+        self.loan_id = None
+        self.member = member
+        self.book = book
+        self.borrowed_date = borrowed_date
+        self.returned_date = None
+    
+    def is_active(self):
+        """Return whether the loan is active or not."""
+        return self.returned_date is None
+    
